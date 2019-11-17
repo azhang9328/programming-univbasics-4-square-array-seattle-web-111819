232086bcf111a3b91e66d9a83801ee71ff511020
Allen Zhang <azhang9328@gmail.com>
2019-11-17 02:09:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0 
   squaredA = []
   while counter < array.length do
-    squaredA. array[counter] ** 2
+    squaredA.push(array[counter] ** 2)
     counter += 1
   end 
+  return squaredA
 end
